da57454a5123493c80482995a2e9f1d654335743
7budi <7budisan@gmail.com>
2023-08-10 10:55:04 -0700

    esh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpa, 
-2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Kor
+pa, 2015-10-19\n", stdout);
 return (1);
 }
