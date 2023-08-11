1698365f919b596afb316029e6ebf035f876defb
7budi <7budisan@gmail.com>
2023-08-11 07:06:46 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	char chh = 'A';
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+for (chh = 'A';chh <= 'Z'; chh++)
+{
+	putchar(chh);
+}
+putchar('\n');
+return (0);
+}
