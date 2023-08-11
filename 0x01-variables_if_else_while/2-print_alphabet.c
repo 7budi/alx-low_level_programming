8f735f3cb796b75cbded0d11fa2a769f40dacace
7budi <7budisan@gmail.com>
2023-08-11 06:35:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alyways 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+for (ch = 'a';ch <= 'z';ch++)
+{
+	putchar(ch);
+}
+return (0);
