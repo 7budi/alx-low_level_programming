409c78b5a1a6dca2ddc7af82fa40ef26c3f7b85c
7budi <7budisan@gmail.com>
2023-08-21 02:45:19 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- * 1-swap = A function that take swaps the taken numbers
+ * swap_int = A function that take swaps the taken numbers
  *
  * @a:given
  * @b:given
  */
 void swap_int(int *a ,int *b)
 {
-	*a = 42;
-	*b = 98;
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
