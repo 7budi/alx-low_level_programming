6374074ff304c446b2c6fda968f952c150f1b1e3
7budi <7budisan@gmail.com>
2023-08-24 05:22:30 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,7 +8,21 @@
  * @n:represents a maximum number of characters to be appended.
  * Return:dest
  */
-char *_strncat(char *dest, char *src, int n){
-    strncat( dest,  src, n);
-    return (dest);
+char *_strncat(char *dest, char *src, int)
+{
+int i, j;
+i = 0;
+j = 0;
+while (dest[i] != "\0")
+{
+	i++;
+}
+while (j < n && src[j] != '\0')
+{
+	dest[i] = src[j];
+	i++;
+	j++;
+}
+dest[i] = '\0';
+return dest;
 }
