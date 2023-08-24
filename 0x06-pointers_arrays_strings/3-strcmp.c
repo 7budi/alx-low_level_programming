e91f7de4991eec0fe7f17cb8dcb95e2b83022e03
7budi <7budisan@gmail.com>
2023-08-24 09:25:58 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ *
+ * @s1:strings
+ * @s2:strings
+ * Return:0
+ */
+int _strcmp(char *s1, char *s2)
+{
+for (int i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+{
+if (s1[i] != s2[i])
+{
+return (s1[i] - s2[i]);
+}
+}
+}
