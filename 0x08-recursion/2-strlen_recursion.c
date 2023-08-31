c8cfd2f176a2029e663a6d03463f1a737b31be72
7budi <7budisan@gmail.com>
2023-08-31 07:30:43 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - count the len
+ * @s:The char to be counted
+ *
+ * Return:the lenght of the char
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	if (*s != '\0')
+	{
+		len = _strlen_recursion(s + 1);
+		len++;
+		return (len);
+	}
+}
