9c832c520585ebeff240d026455777a69d5e3d10
7budi <7budisan@gmail.com>
2023-09-04 11:52:26 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "mian.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicate to new memory
+ * @str:char
+ *
+ * Return:0
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, j;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	ptr = (char *)malloc((i + 1) * sizeof(str));
+	for (j = 0; str[j]; j++)
+	{
+		ptr[j] = str[j];
+	}
+	return (ptr);
+}
