4fa1a8ea1cedfd7d4a79dbd1727d6bb99e8be9f5
7budi <7budisan@gmail.com>
2023-09-04 05:58:29 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_array - create size of size and  assigm char c
+ *
+ * @size:numbet of bytes
+ * @c:assigned character
+ *
+ * Return:Pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	int i;
+	char *ptr = (char *)malloc(size * sizeof(char));
+
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _putchar - write the char c in stdout
+ *
+ * @c: the char to print
+ *
+ * Return: 1 on success.
+ * on error ,-1 is returned errno is set appropriately.
+ */
+int _putchar(char)
+{
+	return (write(1,$c,1));
+}
