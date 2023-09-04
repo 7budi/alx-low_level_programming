96b647a94a2330778de1f734a115a9bd0237ae9a
7budi <7budisan@gmail.com>
2023-09-04 06:50:56 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,12 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *ptr;
 	if (size == 0)
 	{
 		return (NULL);
 	}	
-	char *ptr = (char *)malloc(size * sizeof(char));
+	ptr = (char *)malloc(size * sizeof(char));
 
 	if (ptr == 0)
 	{
