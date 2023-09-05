3b4683e1eb062d49ecec8b0e8a2ac6a22e672ff0
7budi <7budisan@gmail.com>
2023-09-05 07:32:49 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,33 +9,33 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
-	int i, j;
+char *ptr;
+int i, j;
+i = 0;
+j = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0')	
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	ptr = malloc(sizeof(i + j + 1));
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; s1[i]; i++)
-	{
-		ptr[i] = s1[i];
-	}
-	for (j = 0; s2[j]; j++)
-	{
-		ptr[i] = s2[j];
-		i++;
-	}
-	ptr[i] = '\0';
-	return (ptr);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[i] != '\0')
+i++;
+while (s2[j] != '\0')
+j++;
+ptr = malloc(sizeof(i + j + 1));
+if (ptr == NULL)
+{
+return (NULL);
+}
+for (i = 0; s1[i]; i++)
+{
+ptr[i] = s1[i];
+}
+for (j = 0; s2[j]; j++)
+{
+ptr[i] = s2[j];
+i++;
+}
+ptr[i] = '\0';
+return (ptr);
 }
