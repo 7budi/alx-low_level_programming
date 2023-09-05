a3ccc2b138af13c35901fb1b9adddab5e620df6d
7budi <7budisan@gmail.com>
2023-09-05 06:46:08 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,27 +12,22 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int len1 = 0;
 	int len2 = 0;
+	int i, j, s, r;
 
-	if (s1 == NULL | s2 == NULL)
+	if ((s1 == NULL) | (s2 == NULL))
 	{
 		return ("");
 	}
-	int i = 0;
-
 	while (s1[i] != '\0')
 	{
 		i++;
 		len1 = i;
 	}
-	int j = 0;
-
 	while (s2[j] != '\0')
 	{
 		j++;
 		len2 = j;
 	}
-	int s, r;
-
 	ptr = (char *)malloc(sizeof(s1) + 1);
 	if (ptr == NULL)
 	{
