fbfa67bc49ce5d8b783878c38bb8c93e81a0a709
7budi <7budisan@gmail.com>
2023-09-06 07:24:56 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,9 @@
  * @argc: number of arguments
  * @argv: array of argument
  *
- * Return:Always 0
+ * Return:Always 0 (success)
  */
-int main(int argc, char *argv[])
+int main(int argc  __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
