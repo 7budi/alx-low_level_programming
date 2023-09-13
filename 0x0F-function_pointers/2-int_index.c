b2fe2e9699bcf8419a0664e1373d41c8b189767f
7budi <7budisan@gmail.com>
2023-09-13 07:56:34 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,14 +10,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
+	i = 0;
+	if (array == NULL || cmp == NULL || size == NULL)
 	{
 		return(-1);
 	}
-	if (size <= 0)
+	/*if (size <= 0)
 	{
 		return (-1);
-	}
+	}*/
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
