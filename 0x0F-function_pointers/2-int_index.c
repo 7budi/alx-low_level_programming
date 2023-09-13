cd629aa967526df845a19cf2b89c68ed7f4a611c
7budi <7budisan@gmail.com>
2023-09-13 07:50:43 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * int_index - return index place if comparison = true, else -1
+ * @array:array
+ * @size:the size of the array
+ * @cmp:compare
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	i = 0;
+	if (array == NULL || cmp == NULL)
+	{
+		return;
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
